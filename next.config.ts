95ca5333078ca47e65748e7aa563ba2adadc9132
Jose Barrios <jose.barrios@pi.edu.co>
2025-10-06 15:26:48 -0500

    fix: configure outputFileTracingIncludes for db files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    '/app/actions/**': ['./db/**/*.json'],
+    '/app/page': ['./db/**/*.json'],
+  },
 }
 
 export default nextConfig
